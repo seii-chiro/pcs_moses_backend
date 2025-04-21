2bb8503f7ef83b66c597b291ae7199392f9d159d
Steven from WSL <steven.delarosa566@gmail.com>
2025-04-21 23:50:26 +0800

    feat: display updated and created datetime

--- voting/admin.py
+++ voting/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Vote
 
-# Register your models here.
-admin.site.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    list_display = ('id', 'voter_id', 'candidate_id', 'voted_at', 'proxy_for_voter_id', 'is_counted')
+    search_fields = ('voter_id', 'candidate_id', 'proxy_for_voter_id')
+    list_filter = ('is_counted', 'created_at')
+
+admin.site.register(Vote, VoteAdmin)
